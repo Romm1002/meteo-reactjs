f436b46ee8d375903e6e645db878e5a09edf906a
zbrwkq <Damien.chv78@gmail.com>
2023-05-24 15:51:51 +0200

    weatherDisplay v1

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -1,9 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
 const SearchForm = () => {
-    return ( 
-        <form>
-            <input type="text" />
-        </form>
-     );
-}
- 
+  const [city, setCity] = useState("");
+
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=daec8b2f6abe31f4fc66091a92a6edda`
+        );
+
+        setWeatherData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (city !== "") {
+      fetchData();
+    }
+  }, [city]);
+
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={city}
+        onChange={handleInputChange}
+        placeholder="Enter a city"
+      />
+      {weatherData && (
+        <div>
+          <h2>Weather Information for {weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default SearchForm;
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return ( 
         <>
             <SearchForm/>
-            <WeatherDisplay/>
+            <WeatherDisplay />
         </>
      );
 }
